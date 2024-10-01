4b7b3c223a632e91e1c543e77dedd5d8f6ccc1a7
Felix Baumann <felix.bau@gmx.de>
2024-10-01 10:47:57 +0200

    update release to next HEAD

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := 57e86b4466225285a143d52ab65afe722b6450b5
+GLUON_GIT_REF := 678435042eb58029f88f5b6de067585e677933b0 # next

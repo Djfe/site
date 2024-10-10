9d0ae37d115c471ee771a491a6553501016a89e7
FFAC <technik@freifunk-aachen.de>
2024-10-10 22:07:05 +0000

    update openwrt, readd dsl, remove upstreamed patches

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := 678435042eb58029f88f5b6de067585e677933b0 # next
+GLUON_GIT_REF := 50f05c262d79ef071ada84466467f11e7dd165ff #678435042eb58029f88f5b6de067585e677933b0 # next

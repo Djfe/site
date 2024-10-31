3fb6067fadf12d67408c7c942bff8058831076b4
FFAC <technik@freifunk-aachen.de>
2024-10-31 10:19:03 +0000

    Update Gluon to main

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := c0cbcde8f3f3e73e11bcbdfbc72b7c244645424a # main
+GLUON_GIT_REF := 19cb769b1114446738159f1e493389b66caf2dbf # main
